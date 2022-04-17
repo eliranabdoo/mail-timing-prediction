080784e0dcd112e74860464399fc6ac7d8608f62
EliranAbdoo <eliranabdoo@gmail.com>
2022-04-18 01:41:20 +0300

    fixed inference

--- inference.py
+++ inference.py
@@ -6,6 +6,7 @@ MODEL_PATH = r"C:\Users\darkr\PycharmProjects\rightbound\outputs\2022-04-18\00-5
 
 
 def load_data():
+    # Don't really use the train data...
     X = load_train_data(*["C:/Users/darkr/PycharmProjects/rightbound/data/eng.csv",
                           "C:/Users/darkr/PycharmProjects/rightbound/data/companies.csv",
                           "C:/Users/darkr/PycharmProjects/rightbound/data/contacts.csv"]).data
@@ -13,7 +14,7 @@ def load_data():
 
 
 def main():
-    x = load_data().iloc[0:1, :]
+    x = load_data()
     with open(MODEL_PATH, 'rb') as f:
         model = dill.load(f)
     hours = predict_hours(model, x)
